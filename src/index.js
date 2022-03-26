a5c0b30e0981583e24fd1bfd93b0bb11869c94e8
Oleg Kuzmin <alex-dust@yandex.ru>
2022-03-26 11:58:51 +0300

    add webpack

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import addButton from './images/add-button.svg';
+import closeIcon from './images/close-icon.svg';
+import deleteButton from './images/delete-button.svg';
+import custo from './images/custo.jpg';
+import likeActive from './images/like_active.svg';
+import like from './images/like.svg';
+import logo from './images/logo.svg';
+import vector from './images/vector.svg';
+const allImages = [
+  { name: 'addButton', image: addButton },
+  { name: 'closeIcon', image: closeIcon },
+  { name: 'deleteButton', image: deleteButton },
+  { name: 'custo', image: custo },
+  { name: 'likeActive', image: likeActive },
+  { name: 'like', image: like },
+  { name: 'logo', image: logo },
+  { name: 'vector', image: vector },
+];
+
+import './pages/index.css';
