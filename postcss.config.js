1bbfcd2b50aab52c671cc755d5af9575c3aaf4e8
Oleg Kuzmin <alex-dust@yandex.ru>
2023-05-05 21:43:02 +0300

    fix webpack

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,13 @@
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
+const autoprefixer = require('autoprefixer'); // подключаем плагин autoprefixer в файл
+const cssnano = require('cssnano'); // подключаем плагин cssnano в файл
 
 module.exports = {
+  // подключаем плагины к PostCSS
   plugins: [
+    // подключаем autoprefixer
     autoprefixer,
-    cssnano({ preset: 'default' })
-  ]
+    // cssnano при подключении нужно передать объект опций
+    // { preset: default } говорит о том, что нужно использовать стандартные настройки минификации
+    cssnano({ preset: 'default' }),
+  ],
 };
